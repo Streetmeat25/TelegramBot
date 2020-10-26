7dade8c5f195175777a34814dddf323ab6471a45
Streetmeat25 <kotpersikchannel@gmail.com>
2020-10-26 23:52:35 +1000

    2

--- src/main.js
+++ src/main.js
@@ -3,15 +3,29 @@ const token = '1153832709:AAGankVoSsB_jd8aDinyVJAGEhCuS9STMN8';
 const bot = new TelegramBot(token, { polling: true });
 const randomImages = require('./images');
 
+let questions = [
+    {
+        title: 'Начать тест на петуха?',
+        buttons: [
+            [{ text: 'ДА', callback_data: '0_1' }],
+            [{ text: 'Я ЗАССА', callbackData: '0_2' }],
+        ],
+        rightAnswer: 1,
+    },
+];
+//msg.chat.id и msg.from.id думаю можно в константы вынести
+
 //Показывает ошибки подробнее
 bot.on('polling_error', (err) => console.log(err));
 
-bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, 'Welcome', {
-        reply_markup: {
-            keyboard: [['/randomPic']],
-        },
-    });
+bot.onText(/\/start_test/, (msg) => {
+    //-- оставлю чтоб просто было
+    // bot.sendMessage(msg.chat.id, 'Здарова', {
+    //     reply_markup: {
+    //         keyboard: ['/randomPic']]
+    //     },
+    // });
+    //---
 });
 
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -20,20 +34,68 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 
     bot.sendMessage(fromId, resp);
 });
-
 bot.on('message', (msg) => {
-    //переписать через кейсы
+    //console.log(msg.text.includes('петух'));
 
-    if (msg.text.toLowerCase() == 'ауепацан') {
-        bot.sendPhoto(
-            msg.chat.id,
-            'https://static.mk.ru/upload/entities/2017/11/15/articles/detailPicture/e1/10/d5/fe/3d6b3059225fdbb457bc8dfffbeedf77.jpg',
-            { caption: 'aye' },
-        );
-    } else bot.sendMessage(msg.chat.id, 'aye');
+    //хызы как это под кейс переписать
+    if (msg.text.toString().toLowerCase().includes('пидор')) {
+        bot.sendMessage(msg.chat.id, 'БАААН');
+    }
+    switch (msg.text.toString().toLowerCase()) {
+        //
+        // case msg.text.includes('ты петух').toString():
+        //     bot.sendMessage(msg.chat.id, 'БАААН');
+        //     break;
+        case 'kick me': //только в группах
+            bot.kickChatMember(msg.chat.id, msg.from.id);
+            break;
+        case 'где чалишь':
+        case 'где сидишь':
+            bot.sendLocation(msg.chat.id, 51.155034, 54.994556);
+            bot.sendMessage(msg.chat.id, 'В Черном дельфине чалю');
+            break;
+
+        case 'ауе пацан':
+            bot.sendPhoto(
+                msg.chat.id,
+                'https://static.mk.ru/upload/entities/2017/11/15/articles/detailPicture/e1/10/d5/fe/3d6b3059225fdbb457bc8dfffbeedf77.jpg',
+                { caption: 'aye' },
+            );
+            break;
+        case 'йоу':
+        case 'hi':
+        case 'хай':
+            bot.sendMessage(msg.chat.id, 'С пидорами не общаюсь');
+            break;
+        case 'вечер в хату':
+            bot.sendMessage(
+                msg.chat.id,
+                'Часик в радость ' +
+                    msg.from.first_name +
+                    '. Чифир тебе в сладость',
+            );
+            break;
+        case 'здарова':
+        case 'привет':
+            bot.sendMessage(msg.chat.id, 'Здарова, бандит! Как житуха?');
+            break;
+        case 'кисилечек':
+            bot.sendVideo(
+                msg.chat.id,
+                'https://cs5-2v4.vkuservideo.net/p7/1f68e18f839d.720.mp4?extra=INu6do6jCEMdY9Uf8AosekYmwyHPLXoZt2aRVlo3pcEN4B2tiRM0Ou4NNH3kfpg5D5hZGxmTuRpie24PLp18cWjROx3C8hyQEvKREMiLT64UvfOn347RD3LW9zOFfiUzc6uCY8s3OlTu8ZCLm64HmnDB3A&c_uniq_tag=JAprUnbusG88Ba6nXVzI_WZLURKAEmvRo8aHUPm62Vw',
+                { caption: 'Хуху. Вот у нас два мальчика катаются, не один' },
+            );
+
+            break;
+        default:
+            bot.sendMessage(
+                msg.chat.id,
+                'Значит говоришь " ' + msg.text + ' ". Да ты петух походу!',
+            );
+    }
 });
 
-bot.onText(/\/randomPic/, (msg) => {
+bot.onText(/\/randompic/, (msg) => {
     let fromId = msg.chat.id;
 
     bot.sendPhoto(
